60014c14b488a9611570602ca779b91c5a8c4ca1
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2021-09-28 19:06:34 +0200

    loading page

---
+++ Page de chargement/app.js
@@ -0,0 +1,34 @@
+var animload = false;
+var animplyd = false;
+
+$(window).load(function(){
+    animload = true;
+    if(animload && animplyd){
+        $(".loader").fadeOut(1000);
+        $("html, body").css("overflow", "auto");
+    }
+});
+
+$(document).ready(function(){
+    var delay = 0;
+    for(var i = 0; i<5; i++){
+        delay = delay+400;
+        animals(i+1, delay)
+    }
+});
+
+function animals(select, delay){
+    setTimeout(function(){
+        if(select != 5){
+            $(".animals:nth-child("+select+")").css("opacity","1");
+            $(".animals:nth-child("+select+")").css("transform","rotate("+((Math.floor(Math.random()*45))-15)+"deg) scale("+(1+Math.random()*0.25)+")");
+        }else{
+            $(".animals:nth-child(5)").css("animation","loading 1.75s infinite ease-in-out");
+            animplyd = true;
+            if(animload && animplyd){
+                $(".loader").fadeOut(1000);
+                $("html, body").css("overflow", "auto");
+            }
+        }
+    },delay);
+}
