5a7dc1374bec4963d3e688da0e2fa60cdc924fad
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2021-10-16 22:40:37 +0200

    up crsl

--- carroussel/app.js
+++ carroussel/app.js
@@ -1,29 +1,25 @@
 $(document).ready(function(){
 
-    let BDD = [];  // Stockage des noms BDD (Utile pour charger les images)
-
     for(let i = 0; i<$(".crsl").length; i++){   // Initialiser tout les carroussels de la page
-        BDD[i] = $(`.crsl:nth-of-type(${i+1})`).attr("id");
-        initCrsl($(`.crsl:nth-of-type(${i+1})`), BDD[i]);
+        let crsl = $(`.crsl:nth-of-type(${i+1})`);
+        initCrsl(crsl, crsl.attr("data-table"));
     }
 
     $(".arrow1").on("click", function(){
-        rotateCrslLeft($(this).parent().parent());
+        let parent = $(this).parent().parent();
+        rotateCrslLeft(parent, parent.attr("data-table"));
     });
     
     $(".arrow2").on("click", function(){
-        rotateCrslRight($(this).parent().parent());
+        let parent = $(this).parent().parent();
+        rotateCrslRight(parent, parent.attr("data-table"));
     });
 
     function rotateCrslLeft(actual, idBDD){ // Rotation à gauche du carroussel
-        actual.find(".crslTitre, .crslDesc").remove();
-        // if(exist) (dans le BDD)
-        actual.append(`<h3 class="crslTitre">Titre mec</h3>`);
-        actual.append(`<p class="crslDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsum voluptatibus numquam sed, eos doloremque natus earum soluta voluptate nisi nulla excepturi totam inventore, cupiditate velit illum minima ea laudantium!</p>`);
-
         let num  = actual.attr("data-num");
         num++;
         actual.attr("data-num", num);
+        txtCrsl(actual, num);
 
         actual.find("#I0").remove();
         for(let i = 1; i < 7; i++){
@@ -33,14 +29,10 @@ $(document).ready(function(){
     }
 
     function rotateCrslRight(actual, idBDD){ // Rotation à droite du carroussel
-        actual.find(".crslTitre, .crslDesc").remove();
-        // if(exist) (dans le BDD)
-        actual.append(`<h3 class="crslTitre">Titre mec</h3>`);
-        actual.append(`<p class="crslDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsum voluptatibus numquam sed, eos doloremque natus earum soluta voluptate nisi nulla excepturi totam inventore, cupiditate velit illum minima ea laudantium!</p>`);
-
         let num  = actual.attr("data-num");
         num--;
         actual.attr("data-num", num);
+        txtCrsl(actual, num);
 
         actual.find("#I6").remove();
         for(let i = 5; i > -1; i--){
@@ -53,10 +45,13 @@ $(document).ready(function(){
         for(let i=0; i<7; i++){
             actual.find(`.crslWrap>.crslImg:nth-child(${i+1})`).css("background-image",`url(https://picsum.photos/${Math.floor(Math.random()*1900)}/1000)`);
         }
-        actual.attr("data-num", 0);
+        actual.attr("data-num", 1);
+        txtCrsl(actual, 1);
+    }
 
-        // if(exist) (dans le BDD)
-        actual.append(`<h3 class="crslTitre">Titre mec</h3>`);
+    function txtCrsl(actual, num){
+        actual.find(".crslTitre, .crslDesc").remove();
+        actual.append(`<h3 class="crslTitre">Titre n°${num}</h3>`);
         actual.append(`<p class="crslDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsum voluptatibus numquam sed, eos doloremque natus earum soluta voluptate nisi nulla excepturi totam inventore, cupiditate velit illum minima ea laudantium!</p>`);
     }
 
