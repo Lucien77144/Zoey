bc7305ee1400adb7494db0ae18ba17af62bd5c17
Matthislebg <matthis.rousselle@gmail.com>
2021-11-21 13:23:46 +0100

    Page inscription et connexion

--- dev_project/view/connectView.php
+++ dev_project/view/connectView.php
@@ -2,17 +2,38 @@
 $pageTitle = "Se connecter";
 ob_start();
 ?>
+<div class="connexion">
+<h1>Connectez vous !</h1>
+    <form class="connectForm">
 
-<form class="connectForm">
-    <label for="pseudo">pseudo</label>
-    <input type="text" name="pseudo" id="pseudo"> <br>
-    <label for="password">mot de passe</label>
-    <input type="password" name="password" id="password"> <br>
+    <div class="inputContainer">
+        <div class="pseudoContainer">
+            <label for="pseudo">pseudonyme *</label>
+            <input type="text" name="pseudo" id="pseudo" placeholder="pseudo">
+        </div>
+        <div class="passwordContainer">
+             <label for="password">mot de passe *</label>
+            <input type="password" name="password" id="password">
+        </div>
+    </div>
+        
+ 
+        <div class="linksContainer">
+            <input type="submit" id="submitConnect" value="se connecter">
+            <a href="">Pas encore inscrit ?</a>
+        </div>
 
-    <input type="submit" id="submitConnect">
+        <p>*: Champs obligatoires</p>
 </form>
+</div>
+
 <span id="ConfirmationMessage"></span>
+
 
 <?php 
     $viewContent = ob_get_clean();
     require(BASE_URL . 'public/template/template.php');
+
+    // styles BLOCK
+ob_start(); ?>
+<link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_connexion.css">
--- dev_project/view/subscribeView.php
+++ dev_project/view/subscribeView.php
@@ -3,28 +3,77 @@ $pageTitle = "S'inscrire";
 ob_start();
 ?>
 
-<form class="subscribeForm">
-    <label for="pseudo">pseudo</label>
-    <input type="text" name="pseudo" id="pseudoSubscribe"> <br>
+<div class="subscribe">
+        <h1>Bienvenue sur Zoey !</h1>
+        <form class="subscribeForm" action="">
+            
+        
+           <div class="accountContainer">
+               <div class="pseudoContainer">
+                    <label for="pseudo">Pseudonyme *</label>
+                    <input type="text" name="pseudo" id="pseudoSubscribe" placeholder="pseudo" required='required'> 
+               </div>
+                <div class="pictureContainer">
+                    <label for="media">Photo de profil </label>
+                    <input type="file" id="media" name="media" accept="image/png, image/jpeg" >
+                </div>
+           </div>
+        
+           
+            <label for="nom">Nom et prénom *</label>
+            <div class="name">
+            <input type="text" name="nom" id="nom" placeholder="nom" required='required'> 
+            <input type="text" name="prenom" id="prenom" placeholder="prenom" required='required'>   
+            </div>
+           
+            <div class="localisationContainer">
+                <div class="cityContainer">
+                    <label for="ville">Localisation *</label>
+                    <input type="text" name="ville" id="ville" placeholder="ville" required='required'>
+                </div>
+                <div class="codePostalContainer">
+                    <label for="codePostal">Code postal *</label>
+                    <input type="number" name="codePostal" id="codePostal" pattern="[0-9]{5}" placeholder="00000" required='required'>
+                </div>
+            </div>
+                
+            <div class="mailAndBirthContainer">
+                <div class="mailContainer">
+                    <label for="mail">Adresse e-mail *</label>
+                    <input type="email" name="mail" id="mail" placeholder="example@zoey.fr" required='required'>  
+                </div>
+                <div class="dateOfBirthContainer">
+                    <label for="date_naissance">date de naissance *</label>
+                    <input type="date" name="date_naissance" id="date_naissance" required='required'> 
+                </div>
+            </div>
+            
 
-    <label for="media">photo de profil</label>
-    <input type="file" id="media" name="media" accept="image/png, image/jpeg"> <br>
-    
-    <label for="nom">nom</label>
-    <input type="text" name="nom" id="nom"> <br>
-    <label for="prenom">prenom</label>
-    <input type="text" name="prenom" id="prenom"> <br>
-    <label for="mail">mail</label>
-    <input type="email" name="mail" id="mail"> <br>
-    <label for="date_naissance">date de naissance</label>
-    <input type="date" name="date_naissance" id="date_naissance"> <br>
-    <label for="password">mot de passe</label>
-    <input type="password" name="password" id="password"> <br>
+            <label for="password">mot de passe</label>
+            <input type="password" name="password" id="password" required='required'> 
+        
+            <div class="checkboxContainer">
+                <input type="checkbox" name="checkbox" required='required'>
+                <label for="checkbox">Je certifie être majeur, ou dans le cas contraire, j’utilise ZOEY sous la supervision d’un responsable légale.</label>
+            </div>
+            
+            <div class="linksContainer">
+                <input type="submit" id="submitSubscribe" value="s'inscrire">
+                <a href="">Déjà inscrit ?</a>
+            </div>
 
-    <input type="submit" id="submitSubscribe">
-</form>
+            <p>*: Champs obligatoires</p>
+            
+        </form>
+    </div>
 <span id="ConfirmationMessage"></span>
 
 <?php 
+    // styles BLOCK
+ob_start(); ?>
+<link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_subscription.css">
+<?php
+$stylesBlock = ob_get_clean();
+
     $viewContent = ob_get_clean();
     require(BASE_URL . 'public/template/template.php');
