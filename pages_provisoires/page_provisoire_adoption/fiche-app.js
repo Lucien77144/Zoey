4d3ade9c74e3e7cf7f5061a9efa1759d052b4db0
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2021-11-30 17:26:57 +0100

    push adoption

--- pages_provisoires/page_provisoire_adoption/fiche-app.js
+++ pages_provisoires/page_provisoire_adoption/fiche-app.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.querySelectorAll(".BagdeDesc").forEach(e =>{
-            e.addEventListener("click", function(){
-            if(this.classList.contains("close")){
-                this.classList.remove("close");
+    document.querySelectorAll(".badgeDesc").forEach(e =>{
+        e.addEventListener("click", function(){
+            if(document.querySelector("body").clientWidth>800){
+                if(this.classList.contains("close")){
+                    document.querySelectorAll(".badgeDesc").forEach(e =>{
+                        e.classList.add("close");
+                    })
+                    setTimeout(()=>{
+                        this.classList.remove("close");
+                    }, 500);
+                }else{
+                    this.classList.add("close");
+                }
+                
             }else{
-                this.classList.add("close");
+                if(this.classList.contains("close")){
+                    this.classList.remove("close");
+                }else{
+                    this.classList.add("close");
+                }
             }
         });
     });
