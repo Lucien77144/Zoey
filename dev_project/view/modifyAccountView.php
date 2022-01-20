b31e9d5ec82a44dee4265bc28c995eb986ad83f4
Matthislebg <matthis.rousselle@gmail.com>
2022-01-20 17:35:49 +0100

    CSS modifyAccount

--- dev_project/view/modifyAccountView.php
+++ dev_project/view/modifyAccountView.php
@@ -3,58 +3,54 @@ $pageTitle = "Modifier mon compte";
 ob_start();
 ?>
 
-<h1>Modifier mon compte</h1>
+<h1>Modifier mes informations</h1>
 
-<form>
-    <label for="pseudo">pseudo</label>
-    <input type="text" name="pseudo" id="pseudoSubscribe"> <br>
-    <span id="pseudoConfirmationMessage"></span>
-    <input type="submit" id="submitModifyAccountPseudo">
-</form>
-
-<form>
-    <label for="media">photo de profil</label>
-    <input type="file" id="media" name="media" accept="image/png, image/jpeg"> <br>
-    <input type="submit" id="submitModifyAccountMedia">
-</form>
-
-<form>
-    <label for="nom">nom</label>
-    <input type="text" name="nom" id="nom"> <br>
+<div class="formsContainer">
+    <form>
+    <label for="nom">Nom :</label>
+    <input type="text" name="nom" id="nom" placeholder="Nom">
     <input type="submit" id="submitModifyAccountNom">
 </form>
 
 <form>
-    <label for="prenom">prenom</label>
-    <input type="text" name="prenom" id="prenom"> <br>
+    <label for="prenom">Prénom :</label>
+    <input type="text" name="prenom" id="prenom" placeholder="Prénom"> 
     <input type="submit" id="submitModifyAccountPrenom">
 </form>
 
 <form>
-    <label for="mail">mail</label>
-    <input type="email" name="mail" id="mail"> <br>
+    <label for="mail">Mail :</label>
+    <input type="email" name="mail" id="mail" placeholder="adresse@mail.fr"> 
     <input type="submit" id="submitModifyAccountMail">
 </form>
 
 <form>
-    <label for="date_naissance">date de naissance</label>
-    <input type="date" name="date_naissance" id="date_naissance"> <br>
+    <label for="date_naissance">Date de naissance :</label>
+    <input type="date" name="date_naissance" id="date_naissance"> 
     <input type="submit" id="submitModifyAccountDate">
 </form>
 
 <form>
-    <label for="password">mot de passe</label>
-    <input type="password" name="password" id="password"> <br>
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" id="password" placeholder="mot de passe"> 
 
     <input type="submit" id="submitModifyAccountPassword">
 </form>
 
 <span id="confirmationMessage"></span>
+</div>
 
-<br>
 
-<a href="index.php?action=account">voir mon compte</a>
+<a class="btn" href="index.php?action=account">voir mon compte</a>
+
+
 
 <?php 
     $viewContent = ob_get_clean();
     require(BASE_URL . 'public/template/template.php');
+?>
+<?php 
+    $viewContent = ob_get_clean();
+    
+    ob_start(); ?>
+    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_modifyAccount.css" />
