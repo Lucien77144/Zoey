6709b7e354948c1c75bf3a017e1feb5d25e1d11c
Matthislebg <matthis.rousselle@gmail.com>
2022-01-24 17:09:27 +0100

    Correction liens modify account

--- dev_project/view/modifyAccountView.php
+++ dev_project/view/modifyAccountView.php
@@ -9,39 +9,39 @@ ob_start();
     <form>
     <label for="nom">Nom :</label>
     <input type="text" name="nom" id="nom" placeholder="Nom">
-    <input type="submit" id="submitModifyAccountNom">
+    <input class="submitButton" type="submit" id="submitModifyAccountNom">
 </form>
 
 <form>
     <label for="prenom">Prénom :</label>
     <input type="text" name="prenom" id="prenom" placeholder="Prénom"> 
-    <input type="submit" id="submitModifyAccountPrenom">
+    <input class="submitButton" type="submit" id="submitModifyAccountPrenom">
 </form>
 
 <form>
     <label for="mail">Mail :</label>
     <input type="email" name="mail" id="mail" placeholder="adresse@mail.fr"> 
-    <input type="submit" id="submitModifyAccountMail">
+    <input class="submitButton" type="submit" id="submitModifyAccountMail">
 </form>
 
 <form>
     <label for="date_naissance">Date de naissance :</label>
     <input type="date" name="date_naissance" id="date_naissance"> 
-    <input type="submit" id="submitModifyAccountDate">
+    <input class="submitButton" type="submit" id="submitModifyAccountDate">
 </form>
 
 <form>
     <label for="password">Mot de passe :</label>
     <input type="password" name="password" id="password" placeholder="mot de passe"> 
 
-    <input type="submit" id="submitModifyAccountPassword">
+    <input class="submitButton" type="submit" id="submitModifyAccountPassword">
 </form>
 
 <span id="confirmationMessage"></span>
 </div>
 
 
-<a class="btn" href="index.php?action=account">voir mon compte</a>
+<a class="btn seeAccount" href="index.php?action=account">voir mon compte</a>
 
 
 
