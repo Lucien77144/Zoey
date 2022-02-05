addf0e3adabf3f475857bfe2fc33c087785c1cad
Marin2705 <marin.bouanchaud@gmail.com>
2022-02-05 18:44:07 +0100

    feature: $printLegal = false to hide 'mentions legales'

--- dev_project/public/template/template.php
+++ dev_project/public/template/template.php
@@ -16,21 +16,6 @@
     <title><?= $pageTitle ?></title>
 </head>
 <body>
-    <!-- PAGES EN CONSTRUCTION
-        <header>
-        <a href="index.php?action=connect">connexion</a>
-        <a href="index.php?action=disconnect">déconnexion</a>
-        <a href="index.php?action=subscribe">inscription</a>
-        <a href="index.php?action=blog">blog</a>
-        <a href="index.php?action=forum">forum</a>
-        <a href="index.php?action=feed">feed</a>
-        <a href="index.php?action=adoption">adoption</a>
-        <a href="index.php?action=account">mon compte</a>
-        <a href="index.php?action=addpost">poster</a>
-
-        <a href="index.php?action=messages">messages</a>
-    </header> -->
-
     <!-- Header / Footer -->
     <header><h1><a href="index.php">ZOEY</a></h1>
         <!-- <div class="navbar"><a href="index.php?action=messages"><img src="<?= BASE_URL ?>public/images/icons/msg.svg" alt=""></a><a href="index.php?action=addpost"><img src="<?= BASE_URL ?>public/images/icons/upload.svg" alt=""></a></div> -->
@@ -79,6 +64,8 @@
     
     if (isset($printNewsletter) && !$printNewsletter){
         $printNewsletter = true;
+    } else if ($_SESSION['idUser']) {
+        $printNewsletter = true;
     } else {
         ?>
             <form class="newsletter" id="newsletterForm">
@@ -91,11 +78,17 @@
             </form>
         <?php
     }
-    ?>
 
-    <!-- <div class="legalLink">
-        <a href="index.php?action=legal">Mentions légales</a>
-    </div> -->
+    if (isset($printLegal) && !$printLegal){
+        $printLegal = true;
+    } else {
+        ?>
+            <div class="legalLink">
+                <a href="index.php?action=legal">Mentions légales</a>
+            </div>
+        <?php
+    }
+    ?>
 
     <script src="<?= BASE_URL ?>public/js/post.js"></script>
     <?php if (isset($scriptsBlock)) echo $scriptsBlock ?>
