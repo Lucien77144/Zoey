b4360cbc5abb8f732b83012df636f521bd27da24
Marin2705 <marin.bouanchaud@gmail.com>
2022-02-14 09:10:52 +0100

    WIP

--- dev_project/view/connectView.php
+++ dev_project/view/connectView.php
@@ -12,7 +12,10 @@ ob_start();
         <?php
     }
 ?>
-    <form class="connectForm">
+<div>
+    <div id="googleAuthButton"></div>
+</div>
+<form class="connectForm">
 
     <div class="inputContainer">
         <div class="pseudoContainer">
@@ -40,8 +43,47 @@ ob_start();
 
 <?php 
     $viewContent = ob_get_clean();
-    require(BASE_URL . 'public/template/template.php');
 
     // styles BLOCK
-ob_start(); ?>
-<link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_connexion.css">
+    ob_start(); ?>
+    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_connexion.css">
+<?php
+    $stylesBlock = ob_get_clean();
+
+    // SCRIPT BLOCK
+    ob_start(); ?>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+    <script>
+        function handleCredentialResponse(response) {
+          console.log("Encoded JWT ID token: " + response.credential);
+
+        fetch('model/googleAuth.php', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(response.credential),
+        }).then(response => { 
+            response.json().then(function(data){ 
+                console.log('d ',data);
+                return response;
+            })
+        })
+        }
+        window.onload = function () {
+          google.accounts.id.initialize({
+            client_id: "866214768583-13jeokh10iam9q1chmeiphgok3gbkr1i.apps.googleusercontent.com",
+            callback: handleCredentialResponse
+          });
+          google.accounts.id.renderButton(
+            document.getElementById("googleAuthButton"),
+            { theme: "outline", size: "large" }  // customization attributes
+          );
+          google.accounts.id.prompt(); // also display the One Tap dialog
+        }
+    </script>
+<?php
+    $scriptsBlock = ob_get_clean();
+
+    require(BASE_URL . 'public/template/template.php');
