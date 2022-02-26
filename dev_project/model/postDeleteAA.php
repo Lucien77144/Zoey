6659fa213003d04d4d7aa70efbb7adb052835661
Marin2705 <marin.bouanchaud@gmail.com>
2022-02-26 18:31:58 +0100

    feat(account): delete unused photos when upload new profile picture

--- dev_project/model/postDeleteAA.php
+++ dev_project/model/postDeleteAA.php
@@ -4,31 +4,31 @@ session_start();
 require("model.php");
 require("verifyToken.php");
 
-function postDeleteAA(){
+function postDeleteAA()
+{
     $idanimal = safeEntry($_POST['idanimal']);
 
     require("PDO.php");
 
-    $db = new PDO ("mysql:host={$host};dbname={$dbname};", $username, $password, array
-    (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    $db = new PDO("mysql:host={$host};dbname={$dbname};", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
     // get and delete its image
     $sql = "SELECT photo FROM `animal_a_adopter` where idanimal_a_adopter = :idanimal";
-    $req = $db -> prepare($sql);
-    
-    $req -> execute(array(
+    $req = $db->prepare($sql);
+
+    $req->execute(array(
         ':idanimal' => $idanimal
     ));
 
     $photo = $req->fetch();
     $deletePhoto = $photo["photo"];
-    require('deletePhoto.php');
+    unlink('../public/images/upload/' . $deletePhoto);
 
     // delete animal_has_badges (foreign keys)
     $sql = "DELETE FROM `animal_a_adopter_has_badge` WHERE `animal_a_adopter_has_badge`.`animal_a_adopter_idanimal_a_adopter` = :idanimal";
-    $req = $db -> prepare($sql);
-    
-    $req -> execute(array(
+    $req = $db->prepare($sql);
+
+    $req->execute(array(
         ':idanimal' => $idanimal
     ));
 
@@ -37,9 +37,9 @@ function postDeleteAA(){
 
     // delete animal
     $sql = "DELETE FROM `animal_a_adopter` WHERE `animal_a_adopter`.`idanimal_a_adopter` = :idanimal";
-    $req = $db -> prepare($sql);
-    
-    $req -> execute(array(
+    $req = $db->prepare($sql);
+
+    $req->execute(array(
         ':idanimal' => $idanimal
     ));
 
@@ -50,10 +50,11 @@ function postDeleteAA(){
 }
 
 try {
-    if (verifyToken()
-    && $_SESSION['statut'] == 1
-    &&isset($_POST['idanimal'])
-    ){
+    if (
+        verifyToken()
+        && $_SESSION['statut'] == 1
+        && isset($_POST['idanimal'])
+    ) {
         $postDeleteAA = postDeleteAA();
         echo $postDeleteAA;
     } else {
