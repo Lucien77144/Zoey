a3df1b4007f49d91f0c8ba48c0ec4a0f017abc6c
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2022-03-14 10:14:40 +0100

    fix(chat): "voir plus"

--- dev_project/view/chatView.php
+++ dev_project/view/chatView.php
@@ -34,13 +34,16 @@ ob_start();
 <!-- <button class="btn" id="loadMoreMessages">Charger plus</button> -->
 
 <main>
-    <div class="moreMsg">
-        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" id="loadMoreMessages">
-            <rect x="7" width="2" height="16" rx="1" fill="#F3811C" />
-            <rect x="16" y="7" width="2" height="16" rx="1" transform="rotate(90 16 7)" fill="#F3811C" />
-        </svg>
-        <span id="confirmationMessage"></span>
-    </div>
+
+        <?php if($chat){ ?>
+            <div class="moreMsg">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" id="loadMoreMessages">
+                    <rect x="7" width="2" height="16" rx="1" fill="#F3811C" />
+                    <rect x="16" y="7" width="2" height="16" rx="1" transform="rotate(90 16 7)" fill="#F3811C" />
+                </svg>
+                <span id="confirmationMessage"></span>
+            </div>
+        <?php } ?>
 
     <div id="chatContainer">
 
