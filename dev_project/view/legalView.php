ac78aa2ff9097e8d694954f6807b74a460469c1b
Matthislebg <matthis.rousselle@gmail.com>
2022-03-20 14:52:51 +0100

    Added Presentation refuges

--- dev_project/view/legalView.php
+++ dev_project/view/legalView.php
@@ -74,19 +74,131 @@ ob_start();
         pourrait être faite sans le consentement de leurs auteurs ou de leurs
         ayants-droit, est illicite.
     </p>
+    <h1>
+        Conditions générales d’utilisation du site zoey-app.fr
+    </h1>
+    <h2>Évolution et durée des CGU</h2>
+    <p>
+        Les présentes conditions générales d’utilisation sont conclues pour une durée indéterminée. Le contrat produit
+        ses effets à l’égard de l'utilisateur à compter du début de l’utilisation du service. En s’inscrivant sur le
+        service, l’utilisateur accepte sans réserve l’ensemble de ces conditions. Le site zoey-app.fr se réserve le
+        droit de modifier les clauses des conditions générales d’utilisation à tout moment et sans justification.
+    </p>
+    <h2>
+        Restriction d'âge
+    </h2>
+    <p>
+        Les utilisateurs conviennent qu’en utilisant le site zoey-app.fr, ils ont plus de 18 ans. Nous n’assumons aucune
+        responsabilité juridique sur des fausses déclarations d'âge.
+    </p>
+    <h2>Comptes</h2>
+    <p>
+        En créant un compte sur zoey-app.fr, vous acceptez ce qui suit :
+        <ul>
+            <li>que vous êtes seul responsable de votre compte, de la sécurité et de la confidentialité de votre compte.
+            </li>
+            <li>que tous les renseignements personnels sont exacts, véridiques et mis à jour s’ils changent.</li>
+        </ul>
+        Nous nous réservons le droit de suspendre ou de supprimer votre compte si vous violez ces conditions, que vous
+        tentez d’usurper l’identité d’un tiers, ou que vous utilisez une adresse mail qui ne vous appartient pas
+        exclusivement.
+    </p>
+    <h2>
+        Publications par l’Utilisateur
+    </h2>
+    <p>
+        Le site zoey-app.fr permet aux membres de publier des photos et du texte.
+    </p>
+    <p>
+        Nous disposons du droit d’exercer une modération a priori sur les publications et nous pouvons refuser ou
+        annuler leur mise en ligne sans avoir à fournir de justification.
+        Le membre garde l’intégralité de ses droits de propriété intellectuelle. Toutefois, toute publication sur le
+        site implique la délégation du droit non exclusif et gratuit à la société éditrice de représenter, reproduire,
+        modifier, adapter, distribuer et diffuser la publication n’importe où et sur n’importe quel support pour la
+        durée de la propriété intellectuelle. Cela peut se faire directement ou par l’intermédiaire d’un tiers autorisé.
+        Cela concerne notamment le droit d’utilisation de la publication sur le web et sur les réseaux sociaux.
+    </p>
+    <p>
+        L'utilisateur est tenu responsable de tout contenu qu’il met en ligne. L’Utilisateur s’engage à ne pas publier
+        de contenus susceptibles de porter atteinte aux intérêts de tierces personnes. Toutes procédures engagées en
+        justice par un tiers lésé à l’encontre du site devront être prises en charge par l’Utilisateur.
+    </p>
+    <p>
+        La suppression ou la modification par le site du contenu de l’Utilisateur peut s’effectuer à tout moment, pour
+        n’importe quelle raison et sans préavis.
+    </p>
+    <h2>Cookies</h2>
+    <p>
+        Lors des visites sur le site zoey-app.fr, l’installation automatique d’un cookie sur le logiciel de navigation
+        de l’Utilisateur peut survenir.
+    </p>
+    <p>
+        Les cookies correspondent à de petits fichiers déposés temporairement sur le disque dur de l’ordinateur de
+        l’Utilisateur. Ces cookies sont nécessaires pour assurer l’accessibilité et la navigation sur le site web.
+    </p>
+    <p>
+        Les cookies utilisés sur le site Zoey permettent de se connecter à son compte sur Zoey en stockant un
+        identifiant de connexion anonyme (ne comportant aucune donnée personnelle), ou d’afficher des résultats
+        personnalisés sur la page <a
+            href="https://zoey-app.fr/index.php?action=adoption">https://zoey-app.fr/index.php?action=adoption</a> en
+        stockant les réponses au quiz accessible sur la page suivante : <a
+            href="https://zoey-app.fr/index.php?action=quizz">https://zoey-app.fr/index.php?action=quizz</a>
+    </p>
+    <p>
+        En naviguant sur le site, l’Utilisateur accepte les cookies. Leur désactivation peut s’effectuer via les
+        paramètres du logiciel de navigation.
+    </p>
+    <h2>
+        Limites de l’utilisation de la plateforme zoey-app.fr
+    </h2>
+    <p>
+        L’Utilisateur ne doit en aucun cas utiliser la plateforme zoey-app.fr pour
+        <ul>
+            <li>collecter des informations ou des données personnelles sur d’autres utilisateurs de la plateforme,</li>
+            <li>contacter d’autres utilisateurs à des fins commerciales ou de prospection,</li>
+            <li>distribuer des fichiers ou des liens vers des virus ou tout programme informatique nuisible ou
+                préjudiciable aux utilisateurs de la plateforme ou à la plateforme elle-même.</li>
+        </ul>
+    </p>
+    <p>
+        L’Utilisateur ne doit pas porter atteinte à l’intégrité de la plateforme et de son contenu, en tentant
+        <ul>
+            <li>de s’introduire dans les parties protégées de la plateforme et sur les pages d’administration - qui sont
+                interdites d’accès aux utilisateurs du site -,
+            </li>
+            <li>de modifier ou d'altérer le contenu du site en dehors des interfaces prévues à cet effet pour les
+                utilisateurs,
+            </li>
+            <li>
+                de détourner les interfaces prévues pour l’utilisation de la plateforme de leur usage normal,
+            </li>
+            <li>
+                d’introduire d’une quelconque façon des informations, fichiers ou programmes informatiques nuisibles sur
+                la plateforme,
+            </li>
+            <li>
+                de se connecter sur un compte qui appartient à un autre utilisateur,
+            </li>
+            <li>
+                de tenter d’obtenir ou de déchiffrer des échanges chiffrés entre des utilisateurs et la plateforme.
+            </li>
+        </ul>
+    </p>
+    <h2>Droit applicable</h2>
+    <p>L’utilisation de la plateforme Zoey est régie par les présentes Conditions Générales d’Utilisation, ainsi que par le droit français et européen applicable.</p>
 </section>
 <?php
     $viewContent = ob_get_clean();
     
     // styles BLOCK
     ob_start(); ?>
-    <link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_mentions.css" />
+<link rel="stylesheet" href="<?= BASE_URL ?>public/css/style_mentions.css" />
 <?php
     $stylesBlock = ob_get_clean();
 
     // SCRIPT BLOCK
     ob_start(); ?>
-    <!-- <script src="public/js/ YOUR SCRIPT GOES HERE "></script> -->
+<!-- <script src="public/js/ YOUR SCRIPT GOES HERE "></script> -->
 <?php
     $scriptsBlock = ob_get_clean();
 
